4bc2588dfbd185e4f1c5f3261e5c11e4a7278ca4
Shanks <63188620+Shankschn@users.noreply.github.com>
2022-01-17 17:53:46 +0800

    Create remote_local_backup.sh

---
+++ Shell/remote_local_backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#远程备份后 远程机器本地备份 两份备份。
+ 
+SRC_PATH=/data/jira_wiki_backup
+DST_PATH=/data/jira_wiki_backup_old
+SYNC_LOG=/data/sync.log
+rsync -av --ignore-existing --delete ${SRC_PATH}/  ${DST_PATH}/ &>> ${RSYNC_LOG}
